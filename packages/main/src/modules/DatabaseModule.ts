0e73eb3069d18aca45ea18055cd44d65da8a977f
alias-ktc <alenpeni.info@gmail.com>
2026-01-03 16:43:43 +1000

    Begin implementation of DatabaseModule

---
+++ packages/main/src/modules/DatabaseModule.ts
@@ -0,0 +1,36 @@
+import path from "node:path";
+import type { AppModule } from "../AppModule.js";
+import { ModuleContext } from "../ModuleContext.js";
+import { connectDatabase } from "@app/database";
+
+/**
+ * ApplicationDatabase module responsible for applying migrations
+ * and initializing the connection to the Database
+ */
+class ApplicationDatabase implements AppModule {
+  async enable({ app }: ModuleContext): Promise<void> {
+    let sqliteFilePath, migrationsPath: string;
+
+    // Determine paths based on mode
+    switch (app.isPackaged) {
+      case true:
+        sqliteFilePath = path.join(app.getPath("userData"), "database.sqlite");
+        migrationsPath = path.join(process.resourcesPath, "drizzle");
+        break;
+
+      case false:
+        sqliteFilePath = path.join(process.cwd(), "dev.sqlite");
+        migrationsPath = path.resolve("packages/database/drizzle");
+        break;
+    }
+
+    // Debugging information
+    console.log(
+      `\n\nDatabase Path: ${sqliteFilePath}\nMigrations Path: ${migrationsPath}\n\n`
+    );
+  }
+}
+
+export function createDatabaseModule() {
+  return new ApplicationDatabase();
+}
