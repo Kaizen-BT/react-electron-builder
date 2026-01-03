bc86b25803ff7fa5785440f5a6a5bd0836cb14fd
alias-ktc <alenpeni.info@gmail.com>
2026-01-03 16:39:33 +1000

    Added connector and database scripts

---
+++ packages/database/src/index.ts
@@ -0,0 +1,50 @@
+/**
+ * --------------------
+ * Main Database Module
+ * --------------------
+ */
+
+import type { BetterSQLite3Database } from "drizzle-orm/better-sqlite3";
+import Database from "better-sqlite3";
+import { migrate } from "drizzle-orm/better-sqlite3/migrator";
+import { drizzle } from "drizzle-orm/better-sqlite3";
+
+interface SetupDatabaseProps {
+  sqliteFilePath: string;
+  migrationsPath: string;
+  schema: Record<string, unknown>;
+}
+
+export type AppDatabase<T extends Record<string, unknown>> =
+  BetterSQLite3Database<T>;
+
+/**
+ * Sets up the driver for the SQLite database and applies any migrations
+ * needed
+ *
+ * @param {SetupDatabaseProps} props
+ * @returns {Promise<AppDatabase>}
+ */
+export async function connectDatabase({
+  sqliteFilePath,
+  migrationsPath,
+  schema,
+}: SetupDatabaseProps): Promise<AppDatabase<typeof schema>> {
+  // Driver & Setups
+  try {
+    const driver = new Database(sqliteFilePath);
+
+    // FK & Journal mode changes
+    driver.pragma("foreign_keys = ON");
+    driver.pragma("journal_mode = WAL");
+
+    // Setup ORM and apply migrations
+    const db = drizzle(driver, { schema });
+    migrate(db, { migrationsFolder: migrationsPath });
+
+    return db;
+  } catch (error) {
+    console.error(error, "Failed to initialize database");
+    throw new Response("Database error occurred");
+  }
+}
