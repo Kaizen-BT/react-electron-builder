3565006eca098287d98b9c769ed3982e0f6fa1ca
alias-ktc <alenpeni.info@gmail.com>
2026-01-03 14:39:43 +1000

    Integrated react router data-mode

--- packages/renderer/src/main.tsx
+++ packages/renderer/src/main.tsx
@@ -1,10 +1,16 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./routes/App.tsx";
+import { createHashRouter } from "react-router";
+import { RouterProvider } from "react-router";
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+const router = createHashRouter([
+  {
+    path: "/",
+    Component: App,
+  },
+]);
+
+createRoot(document.getElementById("root")!).render(
+  <RouterProvider router={router} />
+);
